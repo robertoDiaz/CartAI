6329e6f385d79a6dae2f1370ba0e01f5ab446554
Roberto Díaz <robertodimen@gmail.com>
2026-06-18 14:33:12 +0200

    Add callbacks to log the errors

--- src/main/java/com/bikemmerce/commerce/infrastructure/out/kafka/CustomerAddedEventPublisherKafkaAdapter.java
+++ src/main/java/com/bikemmerce/commerce/infrastructure/out/kafka/CustomerAddedEventPublisherKafkaAdapter.java
@@ -3,11 +3,13 @@ package com.bikemmerce.commerce.infrastructure.out.kafka;
 import com.bikemmerce.commerce.domain.model.value.objects.CustomerAddedEvent;
 import com.bikemmerce.commerce.domain.ports.events.CustomerAddedEventPublisherPort;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
+@Slf4j
 public class CustomerAddedEventPublisherKafkaAdapter implements CustomerAddedEventPublisherPort {
 
     private static final String TOPIC = "customers-topic";
@@ -15,6 +17,11 @@ public class CustomerAddedEventPublisherKafkaAdapter implements CustomerAddedEve
 
     @Override
     public void added(CustomerAddedEvent event) {
-        kafkaTemplate.send(TOPIC, event.customerId().value(), event);
+        kafkaTemplate.send(TOPIC, event.customerId().value(), event)
+                .whenComplete((stringObjectSendResult, throwable) -> {
+                    if (throwable != null) {
+                        log.error("Error processing customer {}", event.name());
+                    }
+                });
     }
 }
--- src/main/java/com/bikemmerce/commerce/infrastructure/out/kafka/OrderPlacedEventPublisherKafkaAdapter.java
+++ src/main/java/com/bikemmerce/commerce/infrastructure/out/kafka/OrderPlacedEventPublisherKafkaAdapter.java
@@ -3,11 +3,13 @@ package com.bikemmerce.commerce.infrastructure.out.kafka;
 import com.bikemmerce.commerce.domain.model.value.objects.OrderPlacedEvent;
 import com.bikemmerce.commerce.domain.ports.events.OrderPlacedEventPublisherPort;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
+@Slf4j
 public class OrderPlacedEventPublisherKafkaAdapter implements OrderPlacedEventPublisherPort {
 
     private static final String TOPIC = "orders-topic";
@@ -15,6 +17,11 @@ public class OrderPlacedEventPublisherKafkaAdapter implements OrderPlacedEventPu
 
     @Override
     public void publish(OrderPlacedEvent event) {
-        kafkaTemplate.send(TOPIC, event.customerId().value(), event);
+        kafkaTemplate.send(TOPIC, event.customerId().value(), event)
+                .whenComplete((stringObjectSendResult, throwable) -> {
+                    if (throwable != null) {
+                        log.error("Error processing order {}", event.orderId().value());
+                    }
+                });
     }
 }
