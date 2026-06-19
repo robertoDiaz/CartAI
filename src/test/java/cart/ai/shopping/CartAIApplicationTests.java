021596496d071dcb801741122fd2ea56ea5e1ede
Roberto Díaz <robertodimen@gmail.com>
2026-06-19 11:00:11 +0200

    Add docker-compose to simplify

--- src/test/java/cart/ai/shopping/CartAIApplicationTests.java
+++ src/test/java/cart/ai/shopping/CartAIApplicationTests.java
@@ -1,6 +1,6 @@
 /**
- *  Copyright (C) 2026 Roberto Díaz. All rights reserved.
- *  Licensed under the GNU General Public License v3.0. See LICENSE for details.
+ * Copyright (C) 2026 Roberto Díaz. All rights reserved.
+ * Licensed under the GNU General Public License v3.0. See LICENSE for details.
  */
 
 package cart.ai.shopping;
